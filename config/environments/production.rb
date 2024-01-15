e4cef1e3be4b5363bcb001e31dc282606e3c82d6
weijie <jackchong398@gmail.com>
2024-01-16 03:50:38 +0800

    prodction environment changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 require "active_support/core_ext/integer/time"
+require "oyencov"
+OyenCov::Configuration.ENV_PARAMETERS['MODE']="production"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
