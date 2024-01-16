e583f75fc5748b38bbca05411a9d6022a3e4004b
weijie <jackchong398@gmail.com>
2024-01-17 01:18:46 +0800

    update

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require_relative "application"
-require 'oyencov'
 
-OyenCov.new
-ENV['OYENCOV_MODE']='production'
 # Initialize the Rails application.
 Rails.application.initialize!
